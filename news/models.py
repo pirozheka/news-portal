0bff35a1910553de5d7add0df3f9af29413baf68
Jay <you@example.com>
2024-04-27 17:39:24 +0500

    решение в консоли

--- news/models.py
+++ news/models.py
@@ -4,18 +4,21 @@ from django.contrib.auth.models import User
 
 class Author(models.Model):
     author = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_rating = models.DecimalField(default=0, max_digits=3, decimal_places=1)
+    user_rating = models.DecimalField(default=0, max_digits=5, decimal_places=2)
 
-    #Методы
+    def __str__(self):
+        return self.author.username
+    
     def update_rating(self):
         # Суммарный рейтинг каждой статьи автора умножается на 3
-        posts_rate = self.post_set.aggregate(total_rate=Sum('post_rating')*3) or 0
+        posts_rate = self.post_set.aggregate(total_rate=Sum('post_rating'))['total_rate'] or 0
+        posts_rate *= 3
        
         # Суммарный рейтинг всех комментариев автора
-        comments_rate = self.comment_set.aggregate(total_rating=Sum('comment_rating')) or 0
+        comments_rate = self.author.comment_set.aggregate(total_rating=Sum('comment_rating'))['total_rating'] or 0
 
         # Суммарный рейтинг всех комментариев к статьям автора
-        post_comments_rate = Comment.objects.filter(related_post__author=self).aggregate(total_rating=Sum('comment_rating')) or 0
+        post_comments_rate = Comment.objects.filter(related_post__author=self).aggregate(total_rating=Sum('comment_rating'))['total_rating'] or 0
 
         # Обновление рейтинга автора
         self.user_rating = posts_rate + comments_rate + post_comments_rate
