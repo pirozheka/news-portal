ee8ff559903d7a1b6566e24e6d28954d20598f22
Jay <pirozheka@gmail.com>
2024-05-26 22:48:42 +0500

    runapscheduler fix

--- news/management/commands/runapscheduler.py
+++ news/management/commands/runapscheduler.py
@@ -1,5 +1,5 @@
 import logging
-
+from django.contrib.auth.models import User
 from datetime import datetime, timedelta
 from django.template.loader import render_to_string
 from django.utils.timezone import now
@@ -19,20 +19,23 @@ logger = logging.getLogger(__name__)
 
 def send_weekly_newsletter():
     one_week_ago = now() - timedelta(days=7)
-    
-    # Получаем всех подписчиков
-    subscribers = Subscriber.objects.all()
-    
-    for subscriber in subscribers:
-        # Получаем пользователя и категории, на которые он подписан
-        user = subscriber.user
-        
-        # Получаем все посты, опубликованные за последнюю неделю и относящиеся к категориям подписчика
-        posts = Post.objects.filter(post_created__gte=one_week_ago)
 
+    # Получаем все посты, опубликованные за последнюю неделю
+    posts = Post.objects.filter(post_created__gte=one_week_ago)
+
+    # Получаем категории
+    categories = set(post.category_names.values_list('category_name', flat=True) for post in posts)
+    # Получаем список подписчиков
+    users = set(User.objects.filter(subscriber__category__category_name__in=categories))
+
+       
+    for user in users:
+        sub_cat = user.subscriber.values_list('category__category_name', flat=True)
+        posts_cat_sub = posts.filter(category_names__category_name__in=sub_cat)
+        
         # Формируем сообщение для отправки
         message = f"Привет, {user.username}!\n\nНовые посты за неделю:\n\n"
-        for post in posts:
+        for post in posts_cat_sub:
             message += f"{post.post_title}\n"
             message += f"URL: http://127.0.0.1{post.get_absolute_url()}\n\n"
         
@@ -46,7 +49,6 @@ def send_weekly_newsletter():
         )
        
     
-
 
 @util.close_old_connections
 def delete_old_job_executions(max_age=604_800):
@@ -62,7 +64,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             send_weekly_newsletter,
-            trigger=CronTrigger(day_of_week='sun', minute="34", hour="20"),
+            trigger=CronTrigger(day_of_week='fri', minute="00", hour="18"),
             id="my_job",  # The `id` assigned to each job MUST be unique
             max_instances=1,
             replace_existing=True,
@@ -72,7 +74,7 @@ class Command(BaseCommand):
         scheduler.add_job(
             delete_old_job_executions,
             trigger=CronTrigger(
-                day_of_week="mon", hour="00", minute="00"
+                day_of_week="sat", hour="00", minute="00"
             ),
             id="delete_old_job_executions",
             max_instances=1,
